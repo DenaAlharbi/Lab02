0add0720392f86ae29a1d8a3668c84cb66b36fba
Dena Alharbi <109204661+DenaAlharbi@users.noreply.github.com>
2024-09-11 19:09:59 +0300

    Create task1.java

---
+++ task1.java
@@ -0,0 +1,73 @@
+import java.util.LinkedList;
+import java.util.Iterator;
+
+class LinkedListDriver {
+    public static void main(String[] args) {
+        LinkedList<String> stringList = new LinkedList<>();
+
+        stringList.add("Madinah");
+        stringList.add("Dammam");
+        stringList.add("Riyadh");
+        stringList.add("Dhahraan");
+
+        System.out.println("List: " + stringList);
+
+        int index = stringList.indexOf("Riyadh");
+        if(index == -1)
+            System.out.println("Riyadh is not in the list");
+        else{
+            stringList.add(index + 1, "Jubail"); // insert Jubail after Riyadh
+            stringList.addFirst("Najraan");   // insert Najraan at the beginning
+        }
+
+        System.out.println("List: " + stringList);
+
+        index = stringList.indexOf("Dhahraan");
+        if(index == -1)
+            System.out.println("Dhahraan is not in the list");
+        else
+            stringList.set(index, "Abha");   // replace Dhahran with Abha
+
+        String removedString = stringList.removeFirst();
+        System.out.println("Removed Element: " + removedString);
+        stringList.addFirst("Taif");
+        System.out.println("Updated list: " + stringList);
+
+
+        // Add code that will insert "Tabouk" before "Dammam", display error message if
+        // "Dammam" is not in list or if the list is empty. Your code must work for any
+        // two strings str1 and str2.
+
+
+
+
+
+
+
+
+        String str1 = "Tabouk";
+        String str2 = "Dammam";
+        index = stringList.indexOf(str2);
+        if(index == -1)
+            System.out.println(str2 + " is not in the list");
+        else
+            stringList.add(index, str1);
+        System.out.println("Updated list1: " + stringList);
+
+
+
+        // Add code that will insert "AlKhafj" before the last node, your code must work
+        // for any non-empty list. Display an error message if the list is empty.
+
+
+        if(stringList.isEmpty())
+            System.out.println("Empty");
+        else
+            stringList.add(stringList.size() - 1, "AlKhafj");
+
+        System.out.println("Updated list2: " + stringList);
+
+
+
+    }
+}
